b6952d1d1a1af293e653cb107851ca77a46bbf8d
LiuSuny <119320935+LiuSuny@users.noreply.github.com>
2023-12-27 16:54:59 +0300

    Add project files.

---
+++ WinForm_GasoLineStation/Form1.Designer.cs
@@ -0,0 +1,250 @@
+﻿namespace WinForm_GasoLineStation
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.trackBar3 = new System.Windows.Forms.TrackBar();
+            this.trackBar2 = new System.Windows.Forms.TrackBar();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.dataToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.changeLanguageToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.EnglishLang = new System.Windows.Forms.ToolStripMenuItem();
+            this.RusLanguage = new System.Windows.Forms.ToolStripMenuItem();
+            this.ArabicLang = new System.Windows.Forms.ToolStripMenuItem();
+            this.FrenchToolLang = new System.Windows.Forms.ToolStripMenuItem();
+            this.GermanLang = new System.Windows.Forms.ToolStripMenuItem();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.BackColor = System.Drawing.Color.Yellow;
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.trackBar3);
+            this.panel1.Controls.Add(this.trackBar2);
+            this.panel1.Controls.Add(this.trackBar1);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.menuStrip1);
+            this.panel1.Location = new System.Drawing.Point(2, -4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(798, 458);
+            this.panel1.TabIndex = 0;
+            // 
+            // label4
+            // 
+            this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label4.Location = new System.Drawing.Point(94, 54);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(648, 37);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Change of Color Form";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // trackBar3
+            // 
+            this.trackBar3.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.trackBar3.Location = new System.Drawing.Point(55, 240);
+            this.trackBar3.Name = "trackBar3";
+            this.trackBar3.Size = new System.Drawing.Size(731, 45);
+            this.trackBar3.TabIndex = 5;
+            this.trackBar3.Scroll += new System.EventHandler(this.trackBar3_Scroll);
+            // 
+            // trackBar2
+            // 
+            this.trackBar2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.trackBar2.BackColor = System.Drawing.Color.Yellow;
+            this.trackBar2.Location = new System.Drawing.Point(55, 167);
+            this.trackBar2.Name = "trackBar2";
+            this.trackBar2.Size = new System.Drawing.Size(731, 45);
+            this.trackBar2.TabIndex = 5;
+            this.trackBar2.Scroll += new System.EventHandler(this.trackBar2_Scroll);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.trackBar1.BackColor = System.Drawing.Color.Yellow;
+            this.trackBar1.Location = new System.Drawing.Point(55, 94);
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(740, 45);
+            this.trackBar1.TabIndex = 5;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(15, 240);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(14, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "B";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(14, 167);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(15, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "G";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(14, 94);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(15, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "R";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.dataToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(798, 24);
+            this.menuStrip1.TabIndex = 7;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // dataToolStripMenuItem
+            // 
+            this.dataToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.changeLanguageToolStripMenuItem});
+            this.dataToolStripMenuItem.Name = "dataToolStripMenuItem";
+            this.dataToolStripMenuItem.Size = new System.Drawing.Size(43, 20);
+            this.dataToolStripMenuItem.Text = "Data";
+            // 
+            // changeLanguageToolStripMenuItem
+            // 
+            this.changeLanguageToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.EnglishLang,
+            this.RusLanguage,
+            this.ArabicLang,
+            this.FrenchToolLang,
+            this.GermanLang});
+            this.changeLanguageToolStripMenuItem.Name = "changeLanguageToolStripMenuItem";
+            this.changeLanguageToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.changeLanguageToolStripMenuItem.Text = "Change Language";
+            // 
+            // EnglishLang
+            // 
+            this.EnglishLang.Name = "EnglishLang";
+            this.EnglishLang.Size = new System.Drawing.Size(180, 22);
+            this.EnglishLang.Text = "English";
+            this.EnglishLang.Click += new System.EventHandler(this.EnglishLang_Click);
+            // 
+            // RusLanguage
+            // 
+            this.RusLanguage.Name = "RusLanguage";
+            this.RusLanguage.Size = new System.Drawing.Size(180, 22);
+            this.RusLanguage.Text = "Russian";
+            this.RusLanguage.Click += new System.EventHandler(this.RusLanguage_Click);
+            // 
+            // ArabicLang
+            // 
+            this.ArabicLang.Name = "ArabicLang";
+            this.ArabicLang.Size = new System.Drawing.Size(180, 22);
+            this.ArabicLang.Text = "Arabic";
+            // 
+            // FrenchToolLang
+            // 
+            this.FrenchToolLang.Name = "FrenchToolLang";
+            this.FrenchToolLang.Size = new System.Drawing.Size(180, 22);
+            this.FrenchToolLang.Text = "French";
+            this.FrenchToolLang.Click += new System.EventHandler(this.FrenchToolLang_Click);
+            // 
+            // GermanLang
+            // 
+            this.GermanLang.Name = "GermanLang";
+            this.GermanLang.Size = new System.Drawing.Size(180, 22);
+            this.GermanLang.Text = "German";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TrackBar trackBar2;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.TrackBar trackBar3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem dataToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem changeLanguageToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem EnglishLang;
+        private System.Windows.Forms.ToolStripMenuItem RusLanguage;
+        private System.Windows.Forms.ToolStripMenuItem ArabicLang;
+        private System.Windows.Forms.ToolStripMenuItem FrenchToolLang;
+        private System.Windows.Forms.ToolStripMenuItem GermanLang;
+    }
+}
+
---
+++ WinForm_GasoLineStation/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.ProgressBar;
+
+namespace WinForm_GasoLineStation
+{
+    public partial class Form1 : Form
+    {
+       public Form1()
+        {
+            InitializeComponent();
+        }
+       
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            //panel1.BackColor = Color.FromArgb( 0,3, 7, 255);
+            panel1.BackColor = Color.FromArgb(trackBar1.Value, trackBar2.Value, trackBar3.Value);
+            
+
+
+        }
+
+        private void trackBar2_Scroll(object sender, EventArgs e)
+        {
+            //panel1.BackColor = Color.FromArgb( 0,3, 7, 255);
+            panel1.BackColor = Color.FromArgb(trackBar1.Value, trackBar2.Value, trackBar3.Value);
+        }
+
+        private void trackBar3_Scroll(object sender, EventArgs e)
+        {
+            //panel1.BackColor = Color.FromArgb( 0,3, 7, 255);
+            panel1.BackColor = Color.FromArgb(trackBar3.Value, trackBar2.Value, trackBar1.Value);
+        }
+
+        private void RusLanguage_Click(object sender, EventArgs e)
+        {
+            Thread.CurrentThread.CurrentCulture = new System.Globalization.CultureInfo("russian");
+            Thread.CurrentThread.CurrentCulture = new System.Globalization.CultureInfo("russian");
+           
+            System.ComponentModel.ComponentResourceManager resource =
+                new System.ComponentModel.ComponentResourceManager(this.GetType());
+            resource.ApplyResources(this, $"this");
+            foreach (Control c in this.Controls)
+                resource.ApplyResources(c, c.Name);
+        }
+
+        private void EnglishLang_Click(object sender, EventArgs e)
+        {
+        
+           
+            Thread.CurrentThread.CurrentCulture = new System.Globalization.CultureInfo("en");
+            Thread.CurrentThread.CurrentCulture = new System.Globalization.CultureInfo("en");
+           
+            System.ComponentModel.ComponentResourceManager resource =
+                new System.ComponentModel.ComponentResourceManager(this.GetType());
+            resource.ApplyResources(this, $"this");
+            foreach (Control c in this.Controls)
+                resource.ApplyResources(c, c.Name);
+        }
+
+        private void FrenchToolLang_Click(object sender, EventArgs e)
+        {
+            Thread.CurrentThread.CurrentCulture = new System.Globalization.CultureInfo("fr-FR");
+            Thread.CurrentThread.CurrentCulture = new System.Globalization.CultureInfo("fr-FR");
+
+            System.ComponentModel.ComponentResourceManager resource =
+                new System.ComponentModel.ComponentResourceManager(this.GetType());
+            resource.ApplyResources(this, $"this");
+            foreach (Control c in this.Controls)
+                resource.ApplyResources(c, c.Name);
+        }
+    }
+}
